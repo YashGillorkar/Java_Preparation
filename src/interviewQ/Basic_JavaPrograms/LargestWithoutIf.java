b9228217ea8cc54de3fb0580d72b84606f886289
Yash Gillorkar <yashgillorkar02@gmail.com>
2023-11-07 00:05:38 +0530

    Another Program

---
+++ src/interviewQ/Basic_JavaPrograms/LargestWithoutIf.java
@@ -0,0 +1,9 @@
+package interviewQ.Basic_JavaPrograms;
+
+public class LargestWithoutIf {
+    public static void main(String[] args) {
+        int a = 70;
+        int b = 24;
+        System.out.println("Largest is: "+(a>b?a:b));
+    }
+}
