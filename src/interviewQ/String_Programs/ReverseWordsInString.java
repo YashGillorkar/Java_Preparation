8bac61bcfdeaa42f518cbcb85ef8d1b7a3cdf34f
Yash Gillorkar <yashgillorkar02@gmail.com>
2023-11-09 21:44:57 +0530

    String Programs

--- src/interviewQ/String_Programs/ReverseWordsInString.java
+++ src/interviewQ/String_Programs/ReverseWordsInString.java
@@ -1,7 +1,17 @@
+//9. **String Reversal in Words**:
+// Develop a Java program to reverse the words in a given string while maintaining the word order.
+
 package interviewQ.String_Programs;
 
 public class ReverseWordsInString {
     public static void main(String[] args) {
+        String originalString = " I have bachelor degree in computer science";
+        String reverseString = "";
+        String arr[] = originalString.split(" ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            reverseString = reverseString + " " + arr[i];
+        }
+        System.out.println(reverseString);
 
     }
 }
